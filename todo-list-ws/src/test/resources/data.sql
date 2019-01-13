9286efd20ea72a7d8ed1f38c0b96214cbae43433
Guille <guillelo11@gmail.com>
2019-01-13 20:34:39 +0100

    Add new column in test data

--- todo-list-ws/src/test/resources/data.sql
+++ todo-list-ws/src/test/resources/data.sql
@@ -1,2 +1,2 @@
-insert into task values(1, 'Cook pasta');
-insert into task values(2, 'Drink a beer');
+insert into task values(1, 'Cook pasta', false);
+insert into task values(2, 'Drink a beer', true);
