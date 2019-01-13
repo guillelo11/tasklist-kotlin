9b437d372859dd62e3d9df4d24842e1d5e58e01a
Guille <guillelo11@gmail.com>
2019-01-13 13:27:50 +0100

    Add CORS filter

---
+++ todo-list-ws/src/main/kotlin/com/guille/todolistws/config/CorsConfiguration.kt
@@ -0,0 +1,19 @@
+package com.guille.todolistws.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+
+@Configuration
+class CorsConfiguration {
+    @Bean
+    fun corsConfigurer(): WebMvcConfigurer {
+        return object : WebMvcConfigurer {
+            override fun addCorsMappings(registry: CorsRegistry) {
+                registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            }
+        }
+    }
+}
